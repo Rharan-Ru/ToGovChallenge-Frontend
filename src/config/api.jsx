18a70c68e8ed8869be191304e2f2760b505e098b
Rharan-Ru <rharanru@gmail.com>
2024-06-24 09:19:51 -0300

    chore: Add axios instance with interceptors for API requests and responses

---
+++ src/config/api.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
